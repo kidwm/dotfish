2f6597201da8ec07adbc1c4da5347a90d9bc300c
WM Chang <wandererm@gmail.com>
2013-12-25 11:31:53 +0800

    add the color setting for prompt

--- config.fish
+++ config.fish
@@ -1,1 +1,7 @@
 set fish_greeting
+if not set -q __prompt_initialized_2
+  set -U fish_color_user -o green
+  set -U fish_color_host -o cyan
+  set -U fish_color_status red
+  set -U __prompt_initialized_2
+end
